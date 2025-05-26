0df282e4777cf28ea82ae1c11087d49769c7f05d
jaynautic <jaynautic1@gmail.com>
2025-05-26 10:33:37 -0400

    js files

---
+++ content.js
@@ -0,0 +1,30 @@
+const websitesToBlock = [
+    "x.com"
+];
+
+let focusTime = true;
+
+const blockBadSites = async () => {
+    if (focusTime === true) {
+        for (const domain of websitesToBlock) {
+            if (window.location.hostname.includes(domain)) {
+                let newNode = document.createElement("div");
+                newNode.id = "test12345";
+                newNode.style.cssText = "position:absolute;width=100%;height=100%;z-index=100;background-color=blue;"
+                newNode.innerHTML = `<div style="color:white;display:flex;"> get to work </div>`;
+                document.body.style.overflow = "hidden";
+                document.body.appendChild(newNode);
+            }
+        }
+    }
+};
+
+const unblockBadSites = async () => {
+
+};
+
+blockBadSites();
+
+chrome.runtime.onMessage.addListener((message) => {
+
+});
